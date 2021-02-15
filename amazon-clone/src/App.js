b25a923be7121352641999dc3635f1271b78c237
dannythedeveloper1 <daniel.girma9@gmail.com>
2021-02-15 00:59:50 -0500

    checkout-subtotal

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import Header from './Header';
-import Home from './Home';
-
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from './Checkout'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home/>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/">
+					<Header />
+					<Home />
+				</Route>
+				<Route exact path="/Checkout">
+					<Header />
+					<Checkout/>
+				</Route>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Product.css";
-import image from './image.PNG'
-const Product = () => {
+const Product = ({id,title,price,image,rating}) => {
 	return (
 		<div className="product">
 			<div className="product__info">
-				<p>title</p>
+				<p>{title}</p>
 				<p className="product__price">
 					<small>$</small>
-					<strong>price</strong>
+					<strong>{price}</strong>
 				</p>
-				<div className="product__rating">
-					<p>🌟</p>
+                <div className="product__rating">
+                    {Array(rating)
+                        .fill()
+                        .map(()=>(<p>🌟</p>))
+                    }                    
 				</div>
 			</div>
 			<img
